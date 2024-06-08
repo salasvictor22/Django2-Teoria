f8760a3e012588b3006c2beda419c313949b91b3
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-08 12:43:05 -0500

    añadiendo el campo direccion a mi clase persona y agregando un valor por defecto para los objetos que se crearon antes de la modificacion

--- miaplicacion/models.py
+++ miaplicacion/models.py
@@ -4,5 +4,6 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.IntegerField()#(max_digits=3)
+    direccion = models.CharField(max_length=255, default='Sin dirección')
 
  
