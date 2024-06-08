bdcb410215c70f4657cb30f381fc793463d81f23
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-08 13:46:57 -0500

    creando la carpeta templates y creando mi archivo html dentreo de esa carpeta y configurando el archivo setting.py para que pueda funcionar mis plantillas

--- inicio/views.py
+++ inicio/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def myHomeView(*args, **kwargs):
-   return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+def myHomeView(request, *args, **kwargs):
+   print(args, kwargs)
+   print(request.user)
+   return render(request, "home.html",{})
 def anotherView(request):
    return HttpResponse('<h1>Solo otra pagina</h1>') 
