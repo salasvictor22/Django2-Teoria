fe082b79db7f66b84a6a3504e765ec4e07d47d71
salasvictor22 <jsalasag@unsa.edu.pe>
2024-06-08 13:16:07 -0500

    creando una aplicacion llamada inicio, modificando el archivo views.py demi aplicacion y añadiendo el url al archivo urls.py

--- miproyecto/urls.py
+++ miproyecto/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inicio.views import myHomeView
 
 urlpatterns = [
+    path('', myHomeView, name='Pagina de Inicio'),
     path('admin/', admin.site.urls),
 ]
